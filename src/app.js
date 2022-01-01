de6b8f5cb11c656a99d6d2109a7ca84aacb416ad
Nathan Horne <n.horne90@hotmail.co.uk>
2022-01-01 20:44:40 +0000

    Create Reader function/tests added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 // src/app.js
 const express = require("express");
+const readerController = require("./controllers/reader");
+const readerModel = require("./models/reader");
 
 const app = express();
 
 app.use(express.json());
+
+app.post("/reader", readerController.create);
 
 module.exports = app;
---
+++ tests/reader.test.js
@@ -0,0 +1,33 @@
+/* tests/reader.test.js */
+const { expect } = require("chai");
+const request = require("supertest");
+const { Reader } = require("../src/models");
+const app = require("../src/app");
+
+describe("/readers", () => {
+  before(async () => Reader.sequelize.sync());
+
+  beforeEach(async () => {
+    await Reader.destroy({ where: {} });
+  });
+
+  describe("with no records in the database", () => {
+    describe("POST /reader", () => {
+      it("creates a new reader in the database", async () => {
+        const response = await request(app).post("/reader").send({
+          name: "Elizabeth Bennet",
+          email: "future_ms_darcy@gmail.com",
+        });
+
+        const newReaderRecord = await Reader.findByPk(response.body.id, {
+          raw: true,
+        });
+
+        expect(response.body.name).to.equal("Elizabeth Bennet");
+        expect(newReaderRecord.name).to.equal("Elizabeth Bennet");
+        expect(newReaderRecord.email).to.equal("future_ms_darcy@gmail.com");
+        expect(response.status).to.equal(201);
+      });
+    });
+  });
+});
